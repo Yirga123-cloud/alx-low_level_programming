49f14de6ee271c5bae51085a8ae6d7daa0c71cfd
Yirga Kene <yirga2274@gmail.com>
2022-07-06 08:56:40 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets ten times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
