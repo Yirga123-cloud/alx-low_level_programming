28fd016c29ceb6893902930b7ab8b0be93aac61f
Yirga123-cloud <yirga2274@gmail.com>
2022-07-11 10:16:22 -0700

    reset to pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of the address to 98
+ * 
+ * @n: the pointer to the address being reset
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
