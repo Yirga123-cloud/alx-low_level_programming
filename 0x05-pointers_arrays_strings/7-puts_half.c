02960601f8772bbdda5d5b1aa61156a27462ec22
Yirga Kene <yirga2274@gmail.com>
2022-07-11 20:38:34 +0300

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @s: the string
+ */
+void puts_half(char *s)
+{
+	int i = 0, n, j;
+
+	while (s[i] != '\0')
+		i++;
+
+	n = (i - 1) / 2;
+	for (j = n + 1; j < i; j++)
+		_putchar(s[j]);
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
