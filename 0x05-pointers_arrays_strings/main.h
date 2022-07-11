954947eaf0503f5e265465450d6ab3d6f4efc009
Yirga123-cloud <yirga2274@gmail.com>
2022-07-11 10:17:45 -0700

    the main file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+void _putchar(char);
+void reset_to_98(int *);
+void swap_int (int*, int*);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *s);
+void puts_half(char *str);
+
+#endif
